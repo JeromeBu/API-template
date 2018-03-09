f1d7e51a9200a6b56d7f10ae5ccdb71fe8d20ff7
Jerome Burkard <jerome.burkard@gadz.org>
2018-03-09 13:58:58 +0100

    Changing config

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
-require("dotenv").config(); // allows to define env varibles in .env file
-const env = process.env.NODE_ENV || "development";
+require("dotenv").config() // allows to define env varibles in .env file
+const env = process.env.NODE_ENV || "development"
 
 const config = {
   ENV: env,
   MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
   MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN
-};
+}
 
 switch (env) {
   case "development":
-    config.PORT = parseInt(process.env.DEV_APP_PORT) || 3000;
+    config.PORT = parseInt(process.env.DEV_APP_PORT) || 3100
     config.MONGODB_URI =
-      process.env.DEV_MONGODB_URI || "mongodb://localhost:27017/api-template";
-    break;
+      process.env.DEV_MONGODB_URI || "mongodb://localhost:27017/api-template"
+    break
 
   case "test":
-    config.PORT = parseInt(process.env.TEST_APP_PORT) || 3001;
+    config.PORT = parseInt(process.env.TEST_APP_PORT) || 3101
     config.MONGODB_URI =
       process.env.TEST_MONGODB_URI ||
-      "mongodb://localhost:27017/api-template-test";
-    break;
+      "mongodb://localhost:27017/api-template-test"
+    break
 
   case "production":
-    config.PORT = parseInt(process.env.PORT);
-    config.MONGODB_URI = process.env.PROD_MONGODB_URI;
-    break;
+    config.PORT = parseInt(process.env.PORT)
+    config.MONGODB_URI = process.env.PROD_MONGODB_URI
+    break
 
   default:
     console.error(
       `${env} is not a recognized NODE_ENV (only development, test and production are accepted)`
-    );
+    )
 }
 
-module.exports = config;
+module.exports = config
